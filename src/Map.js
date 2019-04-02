54c65571fdaef40bc92f7279b06b76ab9373c85d
Elliyos <k.mccaw25@gmail.com>
2019-04-02 01:30:21 -0500

    fixed map margins

--- src/Map.js
+++ src/Map.js
@@ -5,7 +5,11 @@ const mapStyles = {
     map: {
         position: 'absolute',
         width: '100%',
-        height: '100%'
+        height: '100%',
+        padding: '0',
+        margin: '0',
+        top: '0',
+        left: '0'
     }
 };
 
